cd35007efeeeb2616067879e5c3bec4e0ba13757
shama2025 <2003mashaffer@gmail.com>
2025-04-15 18:52:04 -0400

    Implement speech recognition

--- app/src/main/java/com/mashaffer/mymtgchatbot/MainActivity.kt
+++ app/src/main/java/com/mashaffer/mymtgchatbot/MainActivity.kt
@@ -2,12 +2,37 @@ package com.mashaffer.mymtgchatbot
 
 import Card
 import Rulings
+import android.Manifest
+import android.R.layout
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
+import android.util.LruCache
+import android.view.MotionEvent
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintSet.Motion
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : ComponentActivity(),ScryfallCallback {
+    // UI variables
+    private val micBtn: ImageButton by lazy {findViewById(R.id.micBtn)}
+    private val userTextInput: EditText by lazy {findViewById(R.id.cardInput)}
+
+    // Extra Variables
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private var speechRecognizer: SpeechRecognizer? = null
 
     companion object{
         private const val TAG = "MainActivity"
@@ -19,33 +44,162 @@ class MainActivity : ComponentActivity(),ScryfallCallback {
 
         setContentView(R.layout.main_activity)
 
-        val util: Util = Util()
-        val regex = """(Card name (?<cardName>[\w\s]+))""".toRegex()
-        val result = regex.find("Card name elsha of the infinite")
-        val cardName = result!!.groups["cardName"]?.value
-
-        util.getCardData(MainActivity(),cardName)
-        util.getCardRuleData(MainActivity(),"c0728027-a1ec-4814-87c4-10c3baced0e0")
-        util.getCardSetData(MainActivity(), "5a645837-b050-449f-ac90-1e7ccbf45031")
+        initMainActivity(this)
     }
 
+    private fun initMainActivity(context: Context) {
+        val util: Util = Util()
+
+        val flag = checkSpeechRecognizer(context)
+        if(!flag){
+            Log.i(TAG, "Speech Recognition isn't supported")
+            Toast.makeText(this,"Speech Input is not supported",Toast.LENGTH_LONG).show()
+        }
+        Log.i(TAG, "Supports Speech Recognition")
+
+        requestPermissions()
+
+        onKeyboardListen()
+
+        onSpeechListenerSetup()
+
+        micBtnSpeechListener()
+    }
+
+    private fun micBtnSpeechListener() {
+        micBtn.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    speechRecognizer?.startListening(intent)
+                }
+                MotionEvent.ACTION_UP -> {
+                    speechRecognizer?.stopListening()
+                    micBtn.performClick()
+                }
+            }
+            true
+        }
+    }
+
+
+
+    private fun onSpeechListenerSetup() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        );
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, java.util.Locale.getDefault());
+
+        speechRecognizer?.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(bundle: Bundle) {
+            }
+
+            override fun onBeginningOfSpeech() {
+
+            }
+
+            override fun onRmsChanged(v: Float) {
+            }
+
+            override fun onBufferReceived(bytes: ByteArray) {
+            }
+
+            override fun onEndOfSpeech() {
+            }
+
+            override fun onError(i: Int) {
+            }
+
+            override fun onResults(bundle: Bundle) {
+                val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.get(0)
+                Log.i(TAG, "Data from listener: $data")
+            }
+
+            override fun onPartialResults(bundle: Bundle) {
+            }
+
+            override fun onEvent(i: Int, bundle: Bundle) {
+            }
+        })
+    }
+
+
+    private fun requestPermissions() {
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Mic permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Mic permission denied", Toast.LENGTH_SHORT).show()
+                micBtn.isEnabled = false
+            }
+        }
+
+        // Now actually launch the request
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+        } else {
+            // Permission already granted
+        }
+    }
+
+
+
+
+
+//        val regex = """(Card name (?<cardName>[\w\s]+))""".toRegex()
+//        val result = regex.find("Card name elsha of the infinite")
+//        val cardName = result!!.groups["cardName"]?.value
+
+//        util.getCardData(MainActivity(),cardName)
+//        util.getCardRuleData(MainActivity(),"c0728027-a1ec-4814-87c4-10c3baced0e0")
+//        util.getCardSetData(MainActivity(), "5a645837-b050-449f-ac90-1e7ccbf45031")
+
+
+
+    private fun onKeyboardListen(){
+        userTextInput.setOnKeyListener{v,keycode, event ->
+            if(event.action == android.view.KeyEvent.ACTION_DOWN && keycode == android.view.KeyEvent.KEYCODE_ENTER){
+                Log.i(TAG, "User pressed enter")
+                // Pass the data to the recycler view so it
+                // can append the data to the chat box
+
+                // Then this will also call the util.GetCardGenData
+                true
+            }else{
+                false
+            }
+
+        }
+    }
+
+    /**
+     * This function checks if the given phone supports Speech Recognition
+     */
+    private fun checkSpeechRecognizer(context: Context): Boolean {
+        return SpeechRecognizer.isRecognitionAvailable((context))
+    }
+
+    /**
+     * This function will handle and format the general card data
+     */
     override fun onGetCardData(data: Card?) {
-
-        /**
-         * data?.ruling_uri => If there isn't one save null
-         *      => https://api.scryfall.com/cards/{id = c0728027-a1ec-4814-87c4-10c3baced0e0}/rulings
-         * data?.set_uri => Check if null and if is save null
-         */
-
-        // Will need to save the ruling uri for later.
-        // Will need to save the set uri for later.
-
-
+        val cache = LruCache<String,String?>(2)
+        cache.put("ruling", data?.rulingsUri)
+        cache.put("set", data?.setUri)
         val manaCost = formatManaCost(data?.manaCost)
         val manaColor = formatManaColor(data?.colors)
         val output = "The card ${data?.name} has a base power of ${data?.power} and a base toughness of ${data?.toughness}. " +
                 "It has a mana cost of ${manaCost} and is in the color identity of ${manaColor}." + " ${data?.name} has the ability ${data?.oracleText}."
         Log.i(TAG, output)
+            // Pass the output to the AI recycler view
     }
 
     /**

88592ead013bfde361a63597d37173ab383556e4
shama2025 <2003mashaffer@gmail.com>
2025-07-05 18:34:34 -0400

    Update Android proguard rules *Attempt creating .ipa file for iOS

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mashaffer.mymtgchatbot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.1.0"
+        versionCode = 11
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

5fe742144f28a85b16f1968e05534e02e9f5c508
shama2025 <2003mashaffer@gmail.com>
2025-07-18 12:41:24 -0400

    Update app name *Update build number and app aab

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mashaffer.mymtgchatbot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.1.6"
+        versionCode = 17
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

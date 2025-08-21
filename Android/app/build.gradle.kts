ab3adcb5798bf24bf4ef00956ca32f0e70bad53a
shama2025 <2003mashaffer@gmail.com>
2025-08-20 20:10:28 -0400

    Update App version and release bundle

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mashaffer.mymtgchatbot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.2.0"
+        versionCode = 21
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
